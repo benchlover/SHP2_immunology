efc41af1fea0297052afaee64e43bfd14af8a6e1
benchlover <76722003+benchlover@users.noreply.github.com>
2022-04-06 16:08:38 -0400

    Update R code 1_TIMER.R

--- R code 1_TIMER.R
+++ R code 1_TIMER.R
@@ -1,5 +1,10 @@
 rm(list=ls())
+# introduce the required package: immunedeconv
+# citation for immnuedeconv: https://icbi-lab.github.io/immunedeconv/
 require(immunedeconv)
-a<-read.table("exp.txt", header=TRUE, row.names=1)
-res<-deconvolute(a, "timer", indications=rep("BRCA", ncol(a)))
+# import the mRNA expression data of patients (n=207 following merging with RPPA and mRNA)
+raw<-read.table("exp.txt", header=TRUE, row.names=1)
+# excuete TIMER deconvolution, indicators show that the patients are breast cancer patients
+res<-deconvolute(raw, "timer", indications=rep("BRCA", ncol(raw)))
+# export the results to csv files named result_TIMER
 write.csv(res,"result_TIMER.csv")
